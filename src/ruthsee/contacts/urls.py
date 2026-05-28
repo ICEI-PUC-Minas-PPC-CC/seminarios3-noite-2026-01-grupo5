8f0428bd8e66949aaf8158fbdeb28e265adbc9c2
Fabio Henrique Esprovieri FIlho <jackesprovieri@gmail.com>
2026-05-27 21:34:19 -0300

    Add files via upload

---
+++ src/ruthsee/contacts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'contacts'
+
+urlpatterns = [
+    path('', views.ContactListView.as_view(), name='list'),
+    path('create/', views.ContactCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.ContactUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.ContactDeleteView.as_view(), name='delete'),
+]
